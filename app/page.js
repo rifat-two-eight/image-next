d882751ff7379cd5471c528776677a67d5fa321f
rifat-two-eight <rifat28.dev@gmail.com>
2025-07-15 23:42:01 +0600

    layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 import CardProduct from "./Components/Products/cardProduct";
 
 const  HomePage = async()=> {
-  const res = await fetch('http://localhost:5000/products')
+  const res = await fetch('http://localhost:5000/products',{
+    next:{
+      revalidate:5
+    },
+  })
   const products = await res.json()
   console.log(products)
   return (
     <>
     <h2>Home Page</h2>
-    <div className="grid grid-cols-3 gap-5">
+    <div className="grid grid-cols-3 gap-5 max-w-7xl mx-auto]">
       {
       products.map(product=> <CardProduct key={product.id} product={product}></CardProduct>)
       }
